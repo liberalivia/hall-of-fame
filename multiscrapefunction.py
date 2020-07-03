6d81d12e3173d0cd58dc5ffa88be6f4c2ee91155
liberalivia <67758874+liberalivia@users.noreply.github.com>
2020-07-03 16:57:17 +0100

    Add files via upload
    New version has complete functionality: counts how many games there are on each site, generates the lists to scrape entirely automatically.

--- multiscrapefunction.py
+++ multiscrapefunction.py
@@ -3,46 +3,74 @@ from bs4 import BeautifulSoup
 
 def multiscrape(username):
 
-    def myscrape(site, size):
+    #this first nested function figures out how many versions there are of each puzzle
+    def numberscrape(site):
 
-        # here is the form data
-        data = {
-            'nick' : username,
-            'hallsize' : size
-        }
-
-        # Get the page
-        # use .post
-        # send the data
+        #get the page
         url = "https://www.puzzle-"+site+".com/hall.php"
-        response = requests.post(url, data=data)
-        doc = BeautifulSoup(response.text, 'html.parser')
+        page0 = requests.get(url)
+        doc0 = BeautifulSoup(page0.text, 'html.parser')
 
-        # look for what I want
-        container = doc.find(id="MainContainer")
-        content = container.find(id="pageContent")
+        #find what I want
+        form0 = doc0.find("form")
+        option0 = form0.find_all("option")
 
-        # it is in a table, which I am sure there are easy ways to parse, but I do know that it is always inside the first <th> tag
-        userdata = content.find_all("th")
-        alltime = userdata[0]
+        #count how many options. note to self that this starts from 1 not 0
+        number0 = len(option0)
 
-        # scrape the name of the puzzle
-        puzzle = content.find(class_="on")
+        def myscrape(size):
 
-        #and output!
-        print(puzzle.string, alltime.string)
+            # here is the form data
+            data = {
+                'nick' : username,
+                'hallsize' : size
+            }
 
-    # yes, I could probably still find a way to use a function to generate all these too
-    myscrape("nonograms", 0)
-    myscrape("nonograms", 1)
-    myscrape("nonograms", 2)
-    myscrape("nonograms", 3)
-    myscrape("nonograms", 4)
-    print("\n")
-    myscrape("shingoki", 0)
-    myscrape("shingoki", 1)
-    myscrape("shingoki", 2)
-    myscrape("shingoki", 3)
-    myscrape("shingoki", 4)
+            # I've already got the url, but have to request it again with .post
+            # send the data
+            response = requests.post(url, data=data)
+            doc = BeautifulSoup(response.text, 'html.parser')
+
+            # hah, apparently I didn't need to go down one level at a time, which makes sense
+            # it is in a table, which I am sure there are easy ways to parse, but I do know that it is always inside the first <th> tag
+            userdata = doc.find_all("th")
+            alltime = userdata[0]
+
+            # scrape the name of the puzzle
+            puzzle = doc.find(class_="on")
+
+            #and output!
+            print(puzzle.string,"   ", alltime.string)
+
+        #run the scrape as many times as there are puzzles on that site
+        for i in range(number0):
+            myscrape(i)
+
+    # ok, so now it might still be prettier to have these in some kind of list, but this works
+    numberscrape("shingoki")
+    numberscrape("masyu")
+    numberscrape("stitches")
+    print("aquarium")
+    #this is the site's problem not mine
+    numberscrape("aquarium")
+    numberscrape("tapa")
+    numberscrape("star-battle")
+    numberscrape("kakurasu")
+    numberscrape("skyscrapers")
+    numberscrape("futoshiki")
+    numberscrape("words")
+    numberscrape("shakashaka")
+    numberscrape("kakuro")
+    numberscrape("jigsaw-sudoku")
+    numberscrape("killer-sudoku")
+    numberscrape("binairo")
+    numberscrape("nonograms")
+    numberscrape("loop")
+    numberscrape("sudoku")
+    numberscrape("light-up")
+    numberscrape("bridges")
+    numberscrape("shikaku")
+    numberscrape("nurikabe")
+    numberscrape("dominosa")
 
 multiscrape ("russellan")
